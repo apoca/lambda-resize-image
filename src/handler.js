bc819f54e4350dc4d8dbd91f0b17dd3d8e17e58e
apoca <vieira@esolidar.com>
2020-04-24 19:03:27 +0100

    #28 first refactoring to remove redirects from api gateway.

--- src/handler.js
+++ src/handler.js
@@ -2,7 +2,15 @@
  * Module dependencies.
  */
 
-import { checkKeyExists, getImage } from './lib/image';
+import { S3 as _S3 } from 'aws-sdk';
+import { generateS3Key } from './lib/utils';
+import { isNullOrUndefined } from 'util';
+const sharp = require('sharp');
+
+const S3 = new _S3({
+  signatureVersion: 'v4',
+});
+
 const ALLOWED_DIMENSIONS = {
   width: 1800,
   height: 1800,
@@ -24,12 +32,12 @@ export function imageprocess(event, context, callback) {
   }
 
   const size = {
-    width:
-      queryParameters.width === 'AUTO' ? null : parseInt(queryParameters.width),
-    height:
-      queryParameters.height === 'AUTO'
-        ? null
-        : parseInt(queryParameters.height),
+    width: isNullOrUndefined(queryParameters.width)
+      ? null
+      : parseInt(queryParameters.width),
+    height: isNullOrUndefined(queryParameters.height)
+      ? null
+      : parseInt(queryParameters.height),
   };
 
   if (
@@ -42,19 +50,78 @@ export function imageprocess(event, context, callback) {
     });
   }
 
-  if (!size.width && !size.height) {
-    return getImage(imageKey).catch((err) =>
-      callback(null, {
-        statusCode: err.statusCode || 404,
-        body: JSON.stringify(err),
+  if (imageKey) {
+    if (!size.width && !size.height) {
+      S3.getObject({
+        Bucket: process.env.BUCKET,
+        Key: imageKey,
       })
-    );
+        .promise()
+        .then((data) => sharp(data.Body).jpeg({ quality: 70 }).toBuffer())
+        .then((buffer) => {
+          // generate a binary response with resized image
+          return callback(null, {
+            statusCode: 200,
+            body: buffer.toString('base64'),
+            bodyEncoding: 'base64',
+            isBase64Encoded: true,
+            headers: { 'Content-Type': 'image/jpeg' },
+          });
+        })
+        .catch((err) =>
+          callback(null, {
+            statusCode: err.statusCode || 404,
+            body: JSON.stringify(err),
+          })
+        );
+    } else {
+      S3.getObject({
+        Bucket: process.env.BUCKET,
+        Key: imageKey,
+      })
+        .promise()
+        .then((data) =>
+          sharp(data.Body)
+            .resize(size.width, size.height)
+            .jpeg({ quality: 70 })
+            .toBuffer()
+        )
+        .then((buffer) => {
+          S3.putObject({
+            Body: buffer,
+            Bucket: process.env.BUCKET,
+            ContentType: 'image/jpeg',
+            CacheControl: 'max-age=31536000',
+            Key: generateS3Key(imageKey, size),
+            ACL: 'public-read',
+          })
+            .promise()
+            .catch((err) => {
+              return callback(null, {
+                statusCode: err.statusCode || 404,
+                body: JSON.stringify(err),
+              });
+            });
+          // generate a binary response with resized image
+          return callback(null, {
+            statusCode: 200,
+            body: buffer.toString('base64'),
+            bodyEncoding: 'base64',
+            isBase64Encoded: true,
+            headers: { 'Content-Type': 'image/jpeg' },
+          });
+        })
+        .catch((err) =>
+          callback(null, {
+            statusCode: err.statusCode || 404,
+            body: JSON.stringify(err),
+          })
+        );
+    }
   } else {
-    return checkKeyExists(imageKey, size).catch((err) =>
-      callback(null, {
-        statusCode: err.statusCode || 404,
-        body: JSON.stringify(err),
-      })
-    );
+    return callback(null, {
+      statusCode: 404,
+      body: 'Error: Image not found.',
+    });
   }
 }
